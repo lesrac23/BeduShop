0607b3d4dc28b2ec215508d1933e87adb047feba
lesrac23 <lesrac23@gmail.com>
2022-11-17 02:41:10 -0600

    Prueba para Conexion

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 router.get('/', (req, res) => {
     res.send("Bienvenido al Sistema Drones")
 })
-
+router.use('/productos', require('./producto'));
+router.use('/usuarios', require('./usuario'));
 router.use('/elements', require('./element'));
 router.use('/flightlogs', require('./flightlog'));
 module.exports = router;
